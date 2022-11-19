6355ffaf59064c170941179fa314bf738f2d1098
animashaun87 <animashaunbidemi07@gmail.com>
2022-11-19 13:39:09 +0100

    creating input search box component and filtering the data

--- src/App.js
+++ src/App.js
@@ -8,27 +8,24 @@ class App extends Component {
     this.state = {
       monsters: [],
     };
-
     console.log("constructor");
   }
 
   componentDidMount() {
     console.log("componentDidMount");
     fetch("https://jsonplaceholder.typicode.com/users")
-      .then((res) => {
-        return res.json();
-      })
+      .then((res) => res.json())
       .then((users) =>
-        this.setState(() => {
-          return (
-            {
+        this.setState(
+          () => {
+            return {
               monsters: users,
-            },
-            () => {
-              console.log(this.state);
-            }
-          );
-        })
+            };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
       );
   }
 
@@ -36,6 +33,26 @@ class App extends Component {
     console.log("render");
     return (
       <div className="App">
+        <input
+          type="search"
+          className="search-box"
+          placeholder="search monsters"
+          onChange={(event) => {
+            console.log({ startingArray: this.state.monsters });
+            const searchString = event.target.value.toLocaleLowerCase();
+            const filteredMonsters = this.state.monsters.filter((monster) => {
+              return monster.name.toLocaleLowerCase().includes(searchString);
+            });
+            this.setState(
+              () => {
+                return { monsters: filteredMonsters };
+              },
+              () => {
+                console.log({ endingArray: this.state.monsters });
+              }
+            );
+          }}
+        />
         {this.state.monsters.map((monster) => {
           return (
             <div key={monster.id}>
